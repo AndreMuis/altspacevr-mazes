abfa8351faf73e4d1fe6dd0263362ce60cc8c951
Andre Muis <Andre@AndreMuis.com>
2019-03-16 11:01:03 -0700

    changes

--- built/utility.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class Utility {
-    static distance(x1: number, y1: number, x2: number, y2: number): number;
-    static randomNumber(a: number, b: number): number;
-}
-//# sourceMappingURL=utility.d.ts.map
