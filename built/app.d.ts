95a29b3c3ab48c7d8736f0ec8a899a5770b568a9
Andre Muis <Andre@AndreMuis.com>
2019-03-31 13:56:38 -0700

    changes

--- built/app.d.ts
+++
@@ -1,8 +0,0 @@
-import * as MRESDK from '@microsoft/mixed-reality-extension-sdk';
-export default class Mazes {
-    private context;
-    private baseUrl;
-    constructor(context: MRESDK.Context, baseUrl: string);
-    private started;
-}
-//# sourceMappingURL=app.d.ts.map
