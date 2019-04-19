0a192aa30ec30286036229eef9687425a1851994
Andre Muis <Andre@AndreMuis.com>
2019-04-18 18:42:34 -0700

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ export default class App {
     }
     
     public async started() {  
-        var maze = new Maze(39, 39)
+        var maze = new Maze(9, 9)
         maze.setup()
 
         let renderer = new MazeRenderer(this.context, maze, 3.0) 
