86798f64a8820fc2d4742c5f06cd32c15dabacf0
Andre Muis <Andre@AndreMuis.com>
2019-10-03 15:39:07 -0700

    Update maze-renderer.ts

--- src/maze-renderer.ts
+++ src/maze-renderer.ts
@@ -14,7 +14,7 @@ export class MazeRenderer {
     private maze: Maze
     private scale: number
 
-    private delayInMilliseconds = 20
+    private delayInMilliseconds = 30
 
     static readonly planeZeroScale = 0.001
     static readonly minInterPlanarDistance = 0.005
